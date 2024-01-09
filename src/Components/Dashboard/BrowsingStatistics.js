69ea8846cb96045dddbce25963a07442531de317
Aman Jaiswal <aman.jaiswal@code.berlin>
2024-01-09 01:03:48 +0100

    change the time to reflect local time zone

--- src/Components/Dashboard/BrowsingStatistics.js
+++ src/Components/Dashboard/BrowsingStatistics.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 const BrowsingStatistics = () => {
     const [browsingHistory, setBrowsingHistory] = useState([]);
     const [filterBy, setFilterBy] = useState('mostVisited');
-    const [date, setDate] = useState(new Date().toISOString().split('T')[0]);
+    const [date, setDate] = useState(new Date(Date.UTC(new Date().getFullYear(), new Date().getMonth(), new Date().getDate())).toISOString().split('T')[0]);
 
     useEffect(() => {
         filterBrowsingHistory();
