45fd28041e68e8b5f14324ba7ae5cded0bb3265c
Aman Jaiswal <aman.jaiswal@code.berlin>
2024-01-12 17:15:50 +0100

    Fixed block ad functionality using declarativeNetRequest api

--- src/Components/Customize/ContentSettings.js
+++ src/Components/Customize/ContentSettings.js
@@ -2,32 +2,32 @@ import React, { useState, useEffect } from 'react';
 
 function ContentSettings() {
     const [isHideWidgets, setIsHideWidgets] = useState(false);
-    const [blockPopupsAndAds, setBlockPopupsAndAds] = useState(false);
+    const [blockAds, setblockAds] = useState(false);
 
     useEffect(() => {
         // Code to run on component mount
-        chrome.storage.sync.get(['isHideWidgets', 'blockPopupsAndAds'], ({ isHideWidgets, blockPopupsAndAds }) => {
+        chrome.storage.sync.get(['isHideWidgets', 'blockAds'], ({ isHideWidgets, blockAds }) => {
             setIsHideWidgets(isHideWidgets);
-            setBlockPopupsAndAds(blockPopupsAndAds);
+            setblockAds(blockAds);
         });
     }, []);
 
     useEffect(() => {
         // Code to run when isHideWidgets changes
-        chrome.storage.sync.set({ isHideWidgets: isHideWidgets, blockPopupsAndAds: blockPopupsAndAds });
-    }, [isHideWidgets, blockPopupsAndAds]);
+        chrome.storage.sync.set({ isHideWidgets: isHideWidgets, blockAds: blockAds });
+    }, [isHideWidgets, blockAds]);
 
     return (
         <div>
             <h1>Content Settings</h1>
             <label>
-                Hide recommendation widgets from youtube:
+                Distraction free youtube (hide 'watch next' and ads):
                 <input type="checkbox" checked={isHideWidgets} onChange={() => setIsHideWidgets(!isHideWidgets)} />
             </label>
             <br />
             <label>
-                Block popups and common ads from websites:
-                <input type="checkbox" checked={blockPopupsAndAds} onChange={() => setBlockPopupsAndAds(!blockPopupsAndAds)} />
+                Block common ads from websites:
+                <input type="checkbox" checked={blockAds} onChange={() => setblockAds(!blockAds)} />
             </label>
             <br />
         </div>
