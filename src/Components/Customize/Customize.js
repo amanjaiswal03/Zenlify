ed81fdfbf5b2012699466384dd70c05d611f1e24
Aman Jaiswal <aman.jaiswal@code.berlin>
2024-01-19 02:09:11 +0100

    Redesigned Customize page with material design

--- src/Components/Customize/Customize.js
+++ src/Components/Customize/Customize.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import GeneralSettings from './GeneralSettings';
 import ContentSettings from './ContentSettings';
 import FilterKeywords from './FilterKeywords';
@@ -7,7 +7,6 @@ function Customize() {
 
     return (
         <div>
-            <h1>Customize your web experience</h1>
             <GeneralSettings />
             <ContentSettings />
             <FilterKeywords />
