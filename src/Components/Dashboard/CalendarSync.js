e249f13acbe17e00b059d2cf6166ef3686b7779a
Aman Jaiswal <aman.jaiswal@code.berlin>
2024-01-20 18:48:23 +0100

    Refactored CalendarSync component

--- src/Components/Dashboard/CalendarSync.js
+++ src/Components/Dashboard/CalendarSync.js
@@ -1,120 +1,134 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import SyncIcon from '@mui/icons-material/Sync';
 
 const CalendarSync = () => {
     const [googleSync, setGoogleSync] = useState();
 
+    // Retrieve googleSync state from chrome.storage.sync when the component mounts
     useEffect(() => {
         chrome.storage.sync.get('googleSync', function(result) {
             setGoogleSync(result.googleSync);
         });
     }, []);
 
+    // Save googleSync state to chrome.storage.sync when it changes
     useEffect(() => {
-        chrome.storage.sync.set({ googleSync: googleSync });
+        chrome.storage.sync.set({ googleSync });
     }, [googleSync]);
 
-    function startSync(){
+    // Start syncing with Google Calendar
+    function startSync() {
         addFocusSessionToCalendar();
     }
 
-    function stopSync(){
-        
-        // remove cached auth token
+    // Stop syncing with Google Calendar
+    function stopSync() {
+        removeAuthToken();
+    }
+
+    // Remove cached auth token
+    function removeAuthToken() {
         chrome.identity.getAuthToken({ 'interactive': false }, function(token) {
             if (token) {
-                // remove the token from the cache
-                chrome.identity.removeCachedAuthToken({ 'token': token }, function() {
-                    // revoke the token
-                    var xhr = new XMLHttpRequest();
-                    xhr.open('GET', 'https://accounts.google.com/o/oauth2/revoke?token=' + token);
-                    xhr.send();
-                    console.log('Token revoked and removed.');
+                chrome.identity.removeCachedAuthToken({ token }, function() {
+                    revokeAuthToken(token);
                 });
             }
-            //set chrome storage googleSync key to false
             setGoogleSync(false);
         });
     }
 
+    // Revoke auth token
+    function revokeAuthToken(token) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', `https://accounts.google.com/o/oauth2/revoke?token=${token}`);
+        xhr.send();
+        console.log('Token revoked and removed.');
+    }
+
+    // Add focus session to Google Calendar
     function addFocusSessionToCalendar() {
         chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
             if (chrome.runtime.lastError) {
                 console.log(chrome.runtime.lastError);
                 return;
             }
-            //set chrome storage googleSync key to true
             setGoogleSync(true);
-
-            let focusSessionValues = [];
-            const openRequest = indexedDB.open('focusSessionHistoryDB', 2);
-            openRequest.onsuccess = function (event) {
-                const db = event.target.result;
-                const transaction = db.transaction(['focusSessionHistory'], 'readwrite');
-                const objectStore = transaction.objectStore('focusSessionHistory');
-                const request = objectStore.getAll();
-
-                request.onsuccess = function (event) {
-                    console.log(event);
-                    focusSessionValues = event.target.result;
-                    focusSessionValues?.forEach(session => {
-                        console.log(session);
-                        const event = {
-                            'summary': 'Focus Session',
-                            'description': session.achievement,
-                            'start': {
-                                'dateTime': session.startDateTime,
-                                'timeZone': session.timezoneArea,
-                            },
-                            'end': {
-                                'dateTime': session.endDateTime,
-                                'timeZone': session.timezoneArea,
-                            }
-                        };
-                        const timeMin = encodeURIComponent(session.startDateTime);
-                        const timeMax = encodeURIComponent(session.endDateTime);
-        
-                        fetch(`https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=${timeMin}&timeMax=${timeMax}`, {
-                            method: 'GET',
-                            headers: {
-                                'Authorization': `Bearer ${token}`,
-                                'Content-Type': 'application/json'
-                            }
-                        }).then(response => response.json())
-                        .then(data => {
-                            console.log(data);
-                            const existingEvents = data.items;
-                            console.log(existingEvents);
-                            const isEventExist = existingEvents.some(existingEvent => {
-                                console.log(event.start.dateTime);
-                                return existingEvent.description === event.description;
-                            });
-        
-                            if (isEventExist) {
-                                console.log('Event already exists.');
-                            } else {
-                                fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', {
-                                    method: 'POST',
-                                    headers: {
-                                        'Authorization': `Bearer ${token}`,
-                                        'Content-Type': 'application/json'
-                                    },
-                                    body: JSON.stringify(event)
-                                }).then(response => response.json())
-                                .then(data => {
-                                    console.log('Event created: ' + data.htmlLink);
-                                })
-                                .catch(error => console.error('Error:', error));
-                            }
-                        })
-                        .catch(error => console.error('Error:', error));
-                    });
-                }
-            }
+            getFocusSessionValues(token);
         });
     }
 
+    // Get focus session values from IndexedDB
+    function getFocusSessionValues(token) {
+        const openRequest = indexedDB.open('focusSessionHistoryDB', 2);
+        openRequest.onsuccess = function (event) {
+            const db = event.target.result;
+            const transaction = db.transaction(['focusSessionHistory'], 'readwrite');
+            const objectStore = transaction.objectStore('focusSessionHistory');
+            const request = objectStore.getAll();
+
+            request.onsuccess = function (event) {
+                const focusSessionValues = event.target.result;
+                focusSessionValues?.forEach(session => {
+                    addEventToCalendar(session, token);
+                });
+            }
+        }
+    }
+
+    // Add event to Google Calendar
+    function addEventToCalendar(session, token) {
+        const event = {
+            'summary': 'Focus Session',
+            'description': session.achievement,
+            'start': {
+                'dateTime': session.startDateTime,
+                'timeZone': session.timezoneArea,
+            },
+            'end': {
+                'dateTime': session.endDateTime,
+                'timeZone': session.timezoneArea,
+            }
+        };
+        const timeMin = encodeURIComponent(session.startDateTime);
+        const timeMax = encodeURIComponent(session.endDateTime);
+
+        fetch(`https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=${timeMin}&timeMax=${timeMax}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            }
+        }).then(response => response.json())
+        .then(data => {
+            const existingEvents = data.items;
+            const isEventExist = existingEvents.some(existingEvent => existingEvent.description === event.description);
+
+            if (isEventExist) {
+                console.log('Event already exists.');
+            } else {
+                createEvent(event, token);
+            }
+        })
+        .catch(error => console.error('Error:', error));
+    }
+
+    // Create event in Google Calendar
+    function createEvent(event, token) {
+        fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(event)
+        }).then(response => response.json())
+        .then(data => {
+            console.log('Event created: ' + data.htmlLink);
+        })
+        .catch(error => console.error('Error:', error));
+    }
 
     return (
         <Button variant={googleSync ? "outlined" : "contained"} color="primary" startIcon={<SyncIcon />} onClick={googleSync ? stopSync : startSync}>
