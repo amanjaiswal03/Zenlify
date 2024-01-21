e6ba687402805da44f97cb2462f5bf468d0ade3d
Aman Jaiswal <aman.jaiswal@code.berlin>
2024-01-22 00:21:46 +0100

    fixed default timer

--- public/js/pomodoroTimer.js
+++ public/js/pomodoroTimer.js
@@ -2,7 +2,7 @@
 // Contains the current state and configuration of the Pomodoro timer
 let timerState = {
   countdown: null,
-  timerDuration: 0, // Timer duration in seconds
+  timerDuration: 25 * 60, // Timer duration in seconds
   timerRunning: false, // Indicates if the timer is active
   onBreak: false, // Flag to check if it's currently a break period
   pomodoroDuration: 25 * 60, // Default Pomodoro duration in seconds (25 minutes)
